f3ab5c0d98608ee857760e8a472955bec0c1c999
alisonroot <alisonlroot@gmail.com>
2024-06-25 00:20:13 -0600

    set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-
   return (
     <>
-      <h1>App</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<WarehouseList />} />
+          <Route
+            path="/warehouse/:warehouseId"
+            element={<WarehouseDetails />}
+          />
+          <Route
+            path="/warehouse/:warehouseId/edit"
+            element={<EditWarehouse />}
+          />
+          <Route path="/warehouse/new" element={<AddWarehouse />} />
+          <Route path="/inventory" element={<InventoryList />} />
+          <Route
+            path="/inventory/:inventoryItemId"
+            element={<InventoryItemDetails />}
+          />
+          <Route
+            path="/inventory/:inventoryItemId/edit"
+            element={<InventoryItemDetails />}
+          />
+          <Route path="/inventory/new" element={<AddWarehouse />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
