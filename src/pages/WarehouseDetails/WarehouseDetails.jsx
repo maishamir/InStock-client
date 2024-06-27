308e37575faf5319f8bb1072f256e7fecabd9b6d
alisonroot <alisonlroot@gmail.com>
2024-06-27 09:23:00 -0600

    placeholder text to test

--- src/pages/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/WarehouseDetails/WarehouseDetails.jsx
@@ -4,10 +4,31 @@ import { useParams } from "react-router-dom";
 import { api_URL } from "../../utils/const";
 import "./WarehouseDetails.scss";
 import PageTitle from "../../components/PageTitle/PageTitle";
+// cut for delete model
+import Modal from "react-modal";
+import closeIcon from "../../assets/images/icons/close-24px.svg";
+import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
+import SecondaryButton from "../../components/SecondaryButton/SecondaryButton";
+import DeleteButton from "../../components/DeleteButton/DeleteButton";
 
 function WarehouseDetails() {
   const { warehouseId } = useParams();
   const [warehouse, setWarehouse] = useState(null);
+  // move for delete model
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    //whatever happens goes here
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  // end cut
 
   useEffect(() => {
     if (warehouseId) {
@@ -76,6 +97,60 @@ function WarehouseDetails() {
           </div>
         </div>
       </article>
+      {/* cut for modal */}
+      <div className="delete">
+        <button onClick={openModal}>Replace with delete icon</button>
+        <Modal
+          className="delete__modal"
+          overlayClassName="delete__overlay"
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          contentLabel="Delete Modal"
+        >
+          <button className="delete__close-icon" onClick={closeModal}>
+            <img className="delete__icon" src={closeIcon} alt="close icon" />
+          </button>
+          <div className="delete__content">
+            <div className="delete__text">
+              <h1 className="delete__header">
+                Delete {warehouse_name} Warehouse?
+              </h1>
+              <p className="delete__text">
+                Please confirm that you’d like to delete the {warehouse_name}{" "}
+                from the list of warehouses. You won’t be able to undo this
+                action.
+              </p>
+            </div>
+            <div className="delete__buttons">
+              <SecondaryButton buttonText="Cancel" onClick={closeModal} />
+              <DeleteButton buttonText="Delete" />
+            </div>
+          </div>
+        </Modal>
+      </div>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae id quos
+        asperiores labore blanditiis ducimus earum! Reprehenderit omnis,
+        voluptas facere rem recusandae officia, ab dolor odit enim explicabo
+        eveniet similique? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Obcaecati, vero. Fugit vitae itaque accusamus eveniet animi, rem,
+        inventore deleniti natus veniam atque voluptatem, id consequatur
+        aspernatur totam! Sit, similique quasi. Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Qui accusamus eaque commodi nesciunt
+        ratione perspiciatis quasi non itaque delectus dolores consequatur amet,
+        repellendus nam suscipit sunt. Rem ratione inventore nulla? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Maiores, dolore! Nam,
+        voluptas possimus in est et deserunt ratione commodi sint sit aut autem
+        cupiditate non, consectetur nisi distinctio facilis nobis! Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Nihil dicta est quis
+        quaerat quae eos, ut fugit nobis ipsa, esse ab perferendis, consectetur
+        pariatur omnis voluptates autem incidunt adipisci necessitatibus! Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Perferendis optio in
+        magnam voluptate fugiat quae ea vitae numquam voluptatum alias. Labore
+        optio omnis eius quasi illum alias id consequuntur a.
+      </span>
+      {/* end cut */}
     </main>
   );
 }
