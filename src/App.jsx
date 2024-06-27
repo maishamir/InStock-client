ef793f9fe93f3d736a58d4a88d627324f9f38ec8
alisonroot <alisonlroot@gmail.com>
2024-06-27 10:07:58 -0600

    removed accidental component from app file

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
-        <AddWarehouse />
         <Routes>
           <Route path="/" element={<WarehouseList />} />
           <Route
