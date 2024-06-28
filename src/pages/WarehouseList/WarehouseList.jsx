4e3525d59e15a074f4df54ecae90ab7528434584
maishamir <45680002+maishamir@users.noreply.github.com>
2024-06-28 01:49:24 -0700

    Revert "wrong changes"
    This reverts commit 1f1965ed24c014ea6dac3516913e7f4451f33215.

--- src/pages/WarehouseList/WarehouseList.jsx
+++ src/pages/WarehouseList/WarehouseList.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./WarehouseList.scss";
+import deleteIcon from "../../assets/images/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/images/icons/edit-24px.svg";
 import { api_URL } from "../../utils/const";
 import axios from "axios";
 import PageTitleWithSearch from "../../components/PageTitleWithSearch/PageTitleWithSearch";
@@ -45,6 +47,36 @@ function WarehouseList() {
           </h4>
           <h4 className="warehouse-item__header-label warehouse-item__header-label--align">ACTIONS</h4>
         </div>
+        {/* <div className="warehouse-item__warehouse">
+          <div className="warehouse-item__info warehouse-item__info--warehouse">
+            <h4 className="warehouse-item__label">WAREHOUSE </h4>
+            <p>Manhattan</p>
+          </div>
+
+          <div className="warehouse-item__info warehouse-item__info--address">
+            <h4 className="warehouse-item__label">ADDRESS</h4>
+            <p>503 Broadway, New York, USA</p>
+          </div>
+
+          <div className="warehouse-item__info warehouse-item__info--contact">
+            <h4 className="warehouse-item__label">CONTACT NAME</h4>
+            <p>Parmin Aujla</p>
+          </div>
+
+          <div className="warehouse-item__info warehouse-item__info">
+            <h4 className="warehouse-item__label">CONTACT INFORMATION</h4>
+            <div className="warehouse-item__contact-details">
+              <p className="warehouse-item__tel">+1 (629) 444-0129</p>
+              <p className="warehouse-item__email">paujla@instock.com</p>
+            </div>
+          </div>
+
+          <div class="warehouse-item__info warehouse-item__info--actions">
+            <h4 className="warehouse-item__label">ACTIONS</h4>
+            <img src={deleteIcon} alt="" className="warehouse-item__delete" />
+            <img src={editIcon} alt="" className="warehouse-item__edit" />
+          </div>
+        </div> */}
 
         {warehouseList.map((warehouse) => {
           return <WarehouseItem key={warehouse.id} warehouse={warehouse} />
