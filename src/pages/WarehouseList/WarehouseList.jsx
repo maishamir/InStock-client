58aacb1bf2088aac5d2757107cb9ab8888e7aaa1
maishamir <45680002+maishamir@users.noreply.github.com>
2024-06-28 12:28:16 -0700

    fixed styling on mobile; using DeleteModal component

--- src/pages/WarehouseList/WarehouseList.jsx
+++ src/pages/WarehouseList/WarehouseList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./WarehouseList.scss";
-import deleteIcon from "../../assets/images/icons/delete_outline-24px.svg";
-import editIcon from "../../assets/images/icons/edit-24px.svg";
+
 import { api_URL } from "../../utils/const";
 import axios from "axios";
 import PageTitleWithSearch from "../../components/PageTitleWithSearch/PageTitleWithSearch";
@@ -11,22 +10,27 @@ import sortIcon from "../../assets/images/icons/sort-24px.svg";
 function WarehouseList() {
   const [warehouseList, setWarehouseList] = useState([]);
 
+  const fetchWarehouses = async () => {
+    try {
+      const { data } = await axios.get(`${api_URL}/api/warehouses`);
+      console.log(data);
+      setWarehouseList(data);
+    } catch (e) {
+      console.error("Could not fetch list of warehouses: ", e);
+    }
+  };
+
   useEffect(() => {
-    const fetchWarehouses = async () => {
-      try {
-        const { data } = await axios.get(`${api_URL}/api/warehouses`);
-        console.log(data);
-        setWarehouseList(data);
-      } catch (e) {
-        console.error("Could not fetch list of warehouses: ", e);
-      }
-    };
     fetchWarehouses();
   }, []);
 
   return (
     <section className="warehouse-list">
-      <PageTitleWithSearch title="Warehouses" editLink="#" type="Warehouse" />
+      <PageTitleWithSearch
+        title="Warehouses"
+        addLink="/warehouse/new"
+        type="Warehouse"
+      />
       <div className="warehouse-list__grid">
         <div className="warehouse-list__header">
           <h4 className="warehouse-list__header-label">
@@ -45,10 +49,16 @@ function WarehouseList() {
             CONTACT INFORMATION
             <img src={sortIcon} alt="" className="warehouse-list__icon" />
           </h4>
-          <h4 className="warehouse-list__header-label warehouse-list__header-label--align">ACTIONS</h4>
+          <h4 className="warehouse-list__header-label warehouse-list__header-label--align">
+            ACTIONS
+          </h4>
         </div>
         {warehouseList.map((warehouse) => (
-          <WarehouseItem key={warehouse.id} warehouse={warehouse} />
+          <WarehouseItem
+            key={warehouse.id}
+            warehouse={warehouse}
+            fetchWarehouses={fetchWarehouses}
+          />
         ))}
       </div>
     </section>
