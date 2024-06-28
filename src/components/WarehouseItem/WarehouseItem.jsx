313104ca65c0efe6ecb6d0a1a482679e2dc479f4
maishamir <45680002+maishamir@users.noreply.github.com>
2024-06-28 01:40:11 -0700

    layout working as expected

--- src/components/WarehouseItem/WarehouseItem.jsx
+++ src/components/WarehouseItem/WarehouseItem.jsx
@@ -1,66 +1,43 @@
-import React from "react";
-import "./WarehouseItem.scss";
+import React from 'react'
 import deleteIcon from "../../assets/images/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/images/icons/edit-24px.svg";
-import { Link } from "react-router-dom";
-import arrow from "../../assets/images/icons/chevron_right-24px.svg";
+import { Link } from 'react-router-dom';
+import arrow from "../../assets/images/icons/chevron_right-24px.svg"
 
-function WarehouseItem({
-  address,
-  city,
-  contact_email,
-  contact_name,
-  contact_phone,
-  country,
-  warehouse_name,
-  id
-}) {
-
-  console.log(id)
-
+function WarehouseItem({warehouse}) {
   return (
-    <article className="warehouse-item">
-      <div className="warehouse-item__content">
-        <div className="warehouse-item__left">
-          <div className="warehouse-item__info">
-            <h4 className="warehouse-item__label">WAREHOUSE</h4>
-            <Link className="warehouse-item__link" to={`/warehouse/${id}`}>
-              <p className="warehouse-item__value warehouse-item__name">{warehouse_name}</p>
-              <img src={arrow} alt="" />
-            </Link>
+    <div className="warehouse-item__warehouse">
+          <div className="warehouse-item__info warehouse-item__info--warehouse">
+            <h4 className="warehouse-item__label">WAREHOUSE </h4>
+            <Link to={`/warehouse/${warehouse.id}`}><p className='warehouse-item__warehouse-name'>{warehouse.warehouse_name} <img src={arrow} alt="" className='warehouse-item__icon'/></p></Link>
           </div>
-          <div className="warehouse-item__info">
+
+          <div className="warehouse-item__info warehouse-item__info--address">
             <h4 className="warehouse-item__label">ADDRESS</h4>
-            <p className="warehouse-item__value">
-              {address}, {city}, {country}
-            </p>
+            <p>{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
           </div>
-        </div>
-        <div className="warehouse-item__right">
-          <div className="warehouse-item__info">
+
+          <div className="warehouse-item__info warehouse-item__info--contact">
             <h4 className="warehouse-item__label">CONTACT NAME</h4>
-            <p className="warehouse-item__value">{contact_name}</p>
+            <p>{warehouse.contact_name}</p>
           </div>
-          <div className="warehouse-item__info">
+
+          <div className="warehouse-item__info warehouse-item__info">
             <h4 className="warehouse-item__label">CONTACT INFORMATION</h4>
-            <div className="warehouse-item__contact-info">
-              <p className="warehouse-item__value warehouse-item__tel">
-                {contact_phone}
-              </p>
-              <p className="warehouse-item__value warehouse-item__email">
-                {contact_email}
-              </p>
+            <div className="warehouse-item__contact-details">
+              <p className="warehouse-item__tel">{warehouse.contact_phone}</p>
+              <p className="warehouse-item__email">{warehouse.contact_email}</p>
             </div>
           </div>
-        </div>
-      </div>
 
-      <div className="warehouse-item__actions">
-        <img src={deleteIcon} alt="" />
-        <img src={editIcon} alt="" />
-      </div>
-    </article>
-  );
+          <div class="warehouse-item__info warehouse-item__info--actions">
+            <h4 className="warehouse-item__label">ACTIONS</h4>
+            <img src={deleteIcon} alt="" className="warehouse-item__delete" />
+            <img src={editIcon} alt="" className="warehouse-item__edit" />
+          </div>
+        </div> 
+  )
 }
 
-export default WarehouseItem;
+export default WarehouseItem
+
--- src/pages/WarehouseList/WarehouseList.jsx
+++ src/pages/WarehouseList/WarehouseList.jsx
@@ -1,46 +1,87 @@
 import React, { useEffect, useState } from "react";
 import "./WarehouseList.scss";
+import deleteIcon from "../../assets/images/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/images/icons/edit-24px.svg";
 import { api_URL } from "../../utils/const";
-
-import WarehouseItem from "../../components/WarehouseItem/WarehouseItem";
 import axios from "axios";
-import PageTitleWithSearch from "../../components/PageTitleWithSearch/PageTitleWithSearch"
+import PageTitleWithSearch from "../../components/PageTitleWithSearch/PageTitleWithSearch";
+import WarehouseItem from "../../components/WarehouseItem/WarehouseItem";
+import sortIcon from "../../assets/images/icons/sort-24px.svg";
 
 function WarehouseList() {
-
-  const [warehouseList, setWarehouseList] = useState([])
+  const [warehouseList, setWarehouseList] = useState([]);
 
   useEffect(() => {
     const fetchWarehouses = async () => {
       try {
-        const { data } = await axios.get(`${api_URL}/api/warehouses`)
-        console.log(data)
-        setWarehouseList(data)
+        const { data } = await axios.get(`${api_URL}/api/warehouses`);
+        console.log(data);
+        setWarehouseList(data);
       } catch (e) {
-        console.error("Could not fetch list of warehouses: ", err)
+        console.error("Could not fetch list of warehouses: ", err);
       }
-    }
-    fetchWarehouses()
-  }, [])
+    };
+    fetchWarehouses();
+  }, []);
 
   return (
     <section className="warehouse-list">
       <PageTitleWithSearch title="Warehouses" editLink="#" type="Warehouse" />
-      <header className="warehouse-list__heading">
-        <h4 className="warehouse-list__header-label">WAREHOUSE</h4>
-        <h4 className="warehouse-list__header-label">ADDRESS</h4>
-        <h4 className="warehouse-list__header-label">CONTACT NAME</h4>
-        <h4 className="warehouse-list__header-label">CONTACT INFORMATION</h4>
-      </header>
+      <div className="warehouse-item">
+        <div className="warehouse-item__header">
+          <h4 className="warehouse-item__header-label">
+            WAREHOUSE
+              <img src={sortIcon} alt="" className="warehouse-item__icon" />
+          </h4>
+          <h4 className="warehouse-item__header-label">
+            ADDRESS
+              <img src={sortIcon} alt="" className="warehouse-item__icon" />
+          </h4>
+          <h4 className="warehouse-item__header-label">
+            CONTACT NAME
+              <img src={sortIcon} alt="" className="warehouse-item__icon" />
+          </h4>
+          <h4 className="warehouse-item__header-label">
+            CONTACT INFORMATION
+              <img src={sortIcon} alt="" className="warehouse-item__icon" />
+          </h4>
+          <h4 className="warehouse-item__header-label warehouse-item__header-label--align">ACTIONS</h4>
+        </div>
+        {/* <div className="warehouse-item__warehouse">
+          <div className="warehouse-item__info warehouse-item__info--warehouse">
+            <h4 className="warehouse-item__label">WAREHOUSE </h4>
+            <p>Manhattan</p>
+          </div>
 
-      {
-        warehouseList.map(warehouse => {
-          const { address, city, contact_email, contact_name, contact_phone, country, warehouse_name, id } = warehouse;
+          <div className="warehouse-item__info warehouse-item__info--address">
+            <h4 className="warehouse-item__label">ADDRESS</h4>
+            <p>503 Broadway, New York, USA</p>
+          </div>
 
-          return <WarehouseItem key={id} address={address} city={city} contact_email={contact_email} contact_name={contact_name} contact_phone={contact_phone} country={country} warehouse_name={warehouse_name} id={id} />
-        })
-        
-      }
+          <div className="warehouse-item__info warehouse-item__info--contact">
+            <h4 className="warehouse-item__label">CONTACT NAME</h4>
+            <p>Parmin Aujla</p>
+          </div>
+
+          <div className="warehouse-item__info warehouse-item__info">
+            <h4 className="warehouse-item__label">CONTACT INFORMATION</h4>
+            <div className="warehouse-item__contact-details">
+              <p className="warehouse-item__tel">+1 (629) 444-0129</p>
+              <p className="warehouse-item__email">paujla@instock.com</p>
+            </div>
+          </div>
+
+          <div class="warehouse-item__info warehouse-item__info--actions">
+            <h4 className="warehouse-item__label">ACTIONS</h4>
+            <img src={deleteIcon} alt="" className="warehouse-item__delete" />
+            <img src={editIcon} alt="" className="warehouse-item__edit" />
+          </div>
+        </div> */}
+
+        {warehouseList.map((warehouse) => {
+          return <WarehouseItem key={warehouse.id} warehouse={warehouse} />
+        } )}
+      </div>
     </section>
   );
 }
